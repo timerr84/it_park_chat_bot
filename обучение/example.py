3d0b5e8e5441c3311842bc0ab15efab6c234ddbd
timer <timerr84@mail.ru>
2024-09-18 19:06:04 +0300

    решение прошлой задачи

--- обучение/example.py
+++ обучение/example.py
@@ -1,2 +1,15 @@
-print('Hello it park')
-print('новые данные')
+def sum_list(a):
+    s = 0 #сумма чисел
+    s1 = 0 #длина символов в строке
+    for x in a:
+        if type(x) == int or type(x) == float:
+            s += x
+        elif isinstance(x, str):
+            s1 += len(x)
+        elif type(x) == list:
+            ss, ss1 = sum_list(x)
+            s += ss
+            s1 += ss1
+    return s, s1
+a = [1.2,3,True, 5,7,[12,3, [2,1], 'q'], 'sdfs']
+print(sum_list(a))
